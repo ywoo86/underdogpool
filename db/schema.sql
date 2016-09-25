ad36c74e2261a7dd48c1367966d5c26b53c58bd6
Young Woo <ywoo@umich.edu>
2016-09-25 12:45:08 -0400

    update with partials on loggedin, loggedout, matchups; updates on images and routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
 DROP TABLE IF EXISTS teams;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS matchups;
+DROP TABLE IF EXISTS picks;
 
 CREATE TABLE teams (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   alias VARCHAR(3),
-  img_url VARCHAR(255),
-  palette VARCHAR(10)
+  palette VARCHAR(10),
+  img_url VARCHAR(255)
 );
 
 CREATE TABLE users (
@@ -15,6 +17,21 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE,
   password_digest VARCHAR(255),
   type VARCHAR(10)
+);
+
+CREATE TABLE matchups (
+  id SERIAL PRIMARY KEY,
+  week INTEGER,
+  home_id INTEGER REFERENCES teams(id),
+  away_id INTEGER REFERENCES teams(id)
+);
+
+CREATE TABLE picks (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  pick1_id INTEGER REFERENCES matchups(id),
+  pick2_id INTEGER REFERENCES matchups(id),
+  pick3_id INTEGER REFERENCES matchups(id)
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
 COPY teams (name, alias, img_url, palette) FROM '/Users/youngwoo/Desktop/underdogpool/db/teams.csv' DELIMITER ',' CSV;
 
+COPY matchups (week, home_id, away_id) FROM '/Users/youngwoo/Desktop/underdogpool/db/matchups.csv' DELIMITER ',' CSV;
